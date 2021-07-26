9108e9941c5e60c997805d902a2c4038f3d84f77
SIM WEE TONG RP <19020438@myrp.edu.sg>
2021-07-26 14:21:04 +0800

    added lines testDoAddChromebook(), edited ResourceCentre.java

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -57,14 +57,14 @@ public class ResourceCentreTest {
 		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 		
 		//Given an empty list, after adding 1 item, the size of the list is 1
-		ResourceCentre.addChromebook(chromebookList, cc1);		
+		ResourceCentre.addChromebook(chromebookList, cb1);		
 		assertEquals("Test if that Chromebook arraylist size is 1?", 1, chromebookList.size());
 		
 		//The item just added is as same as the first item of the list
 		assertSame("Test that Chromebook is added same as 1st item of the list?", cc1, chromebookList.get(0));
 		
 		//Add another item. test The size of the list is 2?
-		ResourceCentre.addChromebook(chromebookList, cc2);
+		ResourceCentre.addChromebook(chromebookList, cb2);
 		assertEquals("Test that Chromebook arraylist size is 2?", 2, chromebookList.size());
 
 	}
@@ -97,6 +97,7 @@ public class ResourceCentreTest {
 	public void testRetrieveAllChromebook() {
 		//fail("Not yet implemented");
 		// write your code here
+		
 	}
 
 	@Test
@@ -122,6 +123,7 @@ public class ResourceCentreTest {
 	public void testDoReturnChromebook() {
 		//fail("Not yet implemented");
 		// write your code here
+		String test = "Test";
 	}
 	
 	@After
